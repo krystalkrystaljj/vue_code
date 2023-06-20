0c1c9cbb532dc31d5f01e31ef6d3f3636e6c0360
tjj <394600462@qq.com>
2023-06-20 20:05:51 +0800

    init

---
+++ src/11_动态组件的使用/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button v-for="item in tabs" :key="item"
+            @click="itemClick(item)"
+            :class="{active:currentTab=== item}" >
+      {{ item }}
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tabs:["home","about","category"],
+        currentTab:'home'
+      }
+    },
+    methods:{
+      itemClick(item) {
+        this.currentTab = item
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.active {
+  color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './08_插槽的基本使用/App.vue'
+import App from './11_动态组件的使用/App.vue'
 
 createApp(App).mount('#app')
