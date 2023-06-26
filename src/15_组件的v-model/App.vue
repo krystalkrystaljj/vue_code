3deef8cc037eb377e83a8f9c070cc00869076729
tjj <394600462@qq.com>
2023-06-26 21:20:30 +0800

    23-6-26

---
+++ src/15_组件的v-model/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- input上使用v-model -->
+    <!-- <input v-model="message" type="text">
+    <input :value="message" @input="message = $event.target.value">
+    <h2>{{message}}</h2> -->
+
+
+    <!-- 组件上使用v-model -->
+    <tJJInput v-model="message"></tJJInput>
+    <h2>{{message}}</h2>
+    <!-- <tJJInput :modelValue="message" @update:model-value="message = $event"></tJJInput> -->
+  </div>
+</template>
+
+<script>
+  import tJJInput from './tJJInput'
+  export default {
+    components:{
+      tJJInput
+    },
+    data() {
+      return {
+        message:"hello world"
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './13_引用元素和组件/App'
+import App from './15_组件的v-model/App.vue'
 
 // import {sum} from './12_异步组件的使用/utils/math'
 
